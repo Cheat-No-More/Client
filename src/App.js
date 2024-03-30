3516a2a146765e11847c1625dbfa9932b83a15c2
Balake <126746777+Flameclown@users.noreply.github.com>
2024-03-30 13:38:20 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css'
 import Router from './components/Router'
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 
 
 function App() {
   return (
-    <>
+
       <Router />
-    </>
+
   );
 }
 
--- src/components/Router.js
+++ src/components/Router.js
@@ -9,9 +9,18 @@ import Student from '../pages/Students/Student'
 import MathClass from '../pages/Classes/Math'
 import ScienceClass from '../pages/Classes/Science'
 import EnglishClass from '../pages/Classes/English'
+import Missing from '../pages/Missing'
+import Login from '../pages/Login'
+import Unauthorized from '../pages/Unauthorized'
+import { Routes, Route, Outlet } from 'react-router-dom'
+import RequireAuth from './RequireAuth'
 
+const ROLES = {
+  'User': 2001,
+  'Student': 1984,
+  'Teacher': 5150
+}
 
-import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
 
 export default function Router() {
     const Layout = () => {
@@ -27,21 +36,38 @@ export default function Router() {
 
       const BrowserRoutes = () => {
         return (
-            <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Layout />}>
-                        <Route path="/" element={<Home />} />
+                      {/* public routes */}
                         <Route path="contact-us" element={<Contact />} />
                         <Route path="register" element={<Register />} />
-                        <Route path="studentdashboard" element={<StudentDashboard />} />
+                        <Route path="login" element={<Login />} />
+                        <Route path="unauthorized" element={<Unauthorized />} />
+
+                      {/* private routes */}
+                        <Route element={<RequireAuth allowedRoles={[ROLES.User]}/>}>
+                        <Route path="/" element={<Home />} />
+                        </Route>
+
+                        <Route element={<RequireAuth allowedRoles={[ROLES.Teacher]}/>}>
                         <Route path="teacherdashboard" element={<TeacherDashboard />} />
                         <Route path="classes/math" element={<MathClass />} />
                         <Route path="classes/science" element={<ScienceClass />} />
                         <Route path="classes/english" element={<EnglishClass />} />
                         <Route path="teacherdashboard/student" element={<Student />} />
+                        </Route>
+
+                        <Route element={<RequireAuth allowedRoles={[ROLES.Student]}/>}>
+                        <Route path="studentdashboard" element={<StudentDashboard />} />
+                        <Route path="classes/science" element={<ScienceClass />} />
+                        <Route path="classes/english" element={<EnglishClass />} />
+                        <Route path="classes/math" element={<MathClass />} />
+                        </Route>
+
+                      {/* catch all */}
+                      <Route path="*" element={<Missing />} />
                     </Route>
                 </Routes>
-            </BrowserRouter>
         )
       }
 
